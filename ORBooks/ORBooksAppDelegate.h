b38b13ce39749f61e8e47ff822749eac17fd653b
Xiaochun Shen <snowdonshen@gmail.com>
2011-09-04 21:27:57 +0800

    Initial Commit

---
+++ ORBooks/ORBooksAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ORBooksAppDelegate.h
+//  ORBooks
+//
+//  Created by  Chuns on 11-9-4.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ORBooksAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
